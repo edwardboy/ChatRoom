62980135589ed40240a26203e6c2b91835e5c967
edwardboy <1254339809@qq.com>
2016-08-29 10:48:56 +0800

    Initial Commit

---
+++ ChatRoom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatRoom
+//
+//  Created by Groupfly on 16/8/29.
+//  Copyright © 2016年 Edward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatRoom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatRoom
+//
+//  Created by Groupfly on 16/8/29.
+//  Copyright © 2016年 Edward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
